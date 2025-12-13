bb8c872e68f47f142fb80a404068ee3e75fbe3b6
Kiara <kiabooplal07@gmail.com>
2025-12-13 20:19:54 +0200

    Simple calculator that takes in two numbers and does basic math

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class SimpleCalculator {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int firstNumber = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int secondNumber = input.nextInt();
+
+        input.nextLine();
+        System.out.print("Choose operation + - * /: ");
+        String sign = input.nextLine();
+
+        switch (sign){
+            case "+":
+                System.out.println(firstNumber + secondNumber);
+                break;
+            case "-":
+                System.out.println(firstNumber - secondNumber);
+                break;
+            case "*":
+                System.out.println(firstNumber * secondNumber);
+                break;
+            case "/":
+                System.out.println(firstNumber / secondNumber);
+                break;
+            default: System.out.println("The sign you have entered is invalid");
+        }
+        input.close();
+    }
+}
