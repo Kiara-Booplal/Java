889f9143184cdbeed6c1bc6ad64cea9fae1062db
Kiara <kiabooplal07@gmail.com>
2025-12-13 21:22:39 +0200

    Program that finds the reverses an array

---
+++ src/ReverseArray.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args){
+        int [] array = {1,2,4,5};
+        int [] newArray =  new int[4];
+        int temp=0;
+
+        for(int i = array.length-1; i >= 0; i--){
+           newArray[temp] = array[i];
+           temp ++;
+        }
+        System.out.println(Arrays.toString(newArray));
+    }
+}
