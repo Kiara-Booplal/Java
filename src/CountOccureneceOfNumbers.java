ee09b08e452389857ff92ea1895baead447d0159
Kiara <kiabooplal07@gmail.com>
2025-12-15 19:53:26 +0200

    Program that finds and counts the number of a specific number in an array

---
+++ src/CountOccureneceOfNumbers.java
@@ -0,0 +1,14 @@
+public class CountOccureneceOfNumbers {
+    public static void main(String[] args){
+        int [] array = {1,3,1,6,7,7,7};
+        int search = 7;
+        int count =0;
+        for(int j : array){
+            if(j == search){
+                count++;
+            }
+        }
+        System.out.println("The number you are searching has appeared: " + count + " times");
+
+    }
+}
