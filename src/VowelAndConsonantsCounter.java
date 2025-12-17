fc818b211879dba64f5fa68a326c5b43b5576cc4
Kiara <kiabooplal07@gmail.com>
2025-12-17 15:01:57 +0200

    Counts the number of vowels and consonants ignoring the spaces in a given sentence

---
+++ src/VowelAndConsonantsCounter.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class VowelAndConsonantsCounter {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter in a sentence: ");
+        String sentences = input.nextLine();
+        int countVowel =0;
+        int countConsonants = 0 ;
+        for(int i = 0; i < sentences.length();i++){
+            if(sentences.charAt(i) == 'a' || sentences.charAt(i) == 'e'|| sentences.charAt(i) == 'i' || sentences.charAt(i) == 'o' || sentences.charAt(i) == 'u'){
+                countVowel++;
+            }
+            else if(sentences.charAt(i) != ' '){
+                countConsonants++;
+            }
+        }
+        System.out.println("The number of vowels is: " + countVowel + " and the number of consonants is: " + countConsonants);
+        input.close();
+    }
+}
