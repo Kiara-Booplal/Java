3e87425f8bd0805c7b7eccecc2143af8c4fb7ebe
Kiara <kiabooplal07@gmail.com>
2025-12-18 16:28:39 +0200

    Takes in 5 marks and then gets the average

---
+++ src/AverageOfMarks.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class AverageOfMarks {
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        int [] marksArray = new int[5];
+        int result = 0;
+        for(int i =0; i< marksArray.length; i++){
+            System.out.print("Enter the mark: ");
+            marksArray[i] = input.nextInt();
+            result = result + marksArray[i];
+        }
+        result = result/5;
+        System.out.println("The average is: " + result);
+
+        input.close();
+    }
+}
